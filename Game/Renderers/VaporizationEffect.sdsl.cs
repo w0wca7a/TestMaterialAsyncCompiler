e8d52f53a6e549d55f170a1fac6dd71764026bbc
w0wca7a <154334645+w0wca7a@users.noreply.github.com>
2025-09-28 16:30:08 +0300

    Shader Effect for Additional renderer

---
+++ Game/Renderers/VaporizationEffect.sdsl.cs
@@ -0,0 +1,7 @@
+﻿using Stride.Rendering;
+
+namespace TestMaterialAsyncCompiler.Renderers;
+public static partial class VaporizationEffectKeys
+{
+    public static readonly ObjectParameterKey<float> Time = ParameterKeys.NewObject<float>();
+}
