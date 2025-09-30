db57d9dcba81267b274dd203dbf643666b9fbb9f
w0wca7a <w0wca7a@gmail.com>
2025-09-30 12:29:38 +0300

    Remove not finished renderers to prevent Game Studio crash

--- Game/Renderers/VaporizationEffect.sdsl.cs
+++
@@ -1,7 +0,0 @@
-﻿using Stride.Rendering;
-
-namespace TestMaterialAsyncCompiler.Renderers;
-public static partial class VaporizationEffectKeys
-{
-    public static readonly ObjectParameterKey<float> Time = ParameterKeys.NewObject<float>();
-}
